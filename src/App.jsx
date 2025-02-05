db16cc37036e3565518dee15e989eff637f5e189
niko87dot <niko.schweres@gmail.com>
2025-02-05 10:54:40 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import Home from "./pages/home/Home"
+import About from "./pages/about/About"
+import Contact from "./pages/contact/Contact"
+import Podcast from "./pages/podcast/Podcast"
+import References from "./pages/references/References"
 
 const App = () => {
   return (
     <div>
-      Test
+      <Home/>
+      <About/>
+      <Contact/>
+      <Podcast/>
+      <References/>
     </div>
   )
 }
