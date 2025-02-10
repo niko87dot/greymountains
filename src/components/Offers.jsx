42ab0c15e176fc9e0283164f1f2ed9d8a25f2097
niko87dot <niko.schweres@gmail.com>
2025-02-10 17:34:03 +0100

    Add files via upload

---
+++ src/components/Offers.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BsFillCameraReelsFill } from "react-icons/bs";
+
+
+const Offers = () => {
+  return (
+    <section className="offers">
+        <div className="container offers__container">
+            <div className="offers__head">
+                <span>{<BsFillCameraReelsFill/>}</span>
+                <h2>Platzhalter</h2>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Offers
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from "../../components/MainHeader"
 import "./home.css"
+import Offers from '../../components/Offers'
 
 const Home = () => {
   return (
     <div>
       <>
       <Header/>
-
+      <Offers/>
       </>
     </div>
   )
