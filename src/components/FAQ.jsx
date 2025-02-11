48072577340d455a20d60fac6cc0330988592497
niko87dot <niko.schweres@gmail.com>
2025-02-11 14:11:52 +0100

    Add files via upload

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import {AiOutlinePlus} from "react-icons/ai"
-import {AiOutlineminus} from "react-icons/ai"
+import { useState } from "react"
+import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ai"
 
 
 const FAQ = ({question, answer}) => {
+    const [isAnswerShowing, setIsAnswerShowing] = useState(false);
+
   return (
-    <article className='faq'>
+    <article className='faq' onClick={() => setIsAnswerShowing(prev => !prev)}>
         <div>
             <h4>{question}</h4>
             <button className='faq__icon'>
-                <AiOutlinePlus />
+                {
+                    isAnswerShowing ? <AiOutlineMinus/> : <AiOutlinePlus/>
+                }
             </button>
         </div>
+        {isAnswerShowing && <p>{answer}</p>}
     </article>
   )
 }
