e02bb5a83be70c812bf46e0d4a95ef30f66d9c05
niko87dot <niko.schweres@gmail.com>
2025-02-14 17:47:43 +0100

    Add files via upload

--- src/pages/podcast/Podcast.jsx
+++ src/pages/podcast/Podcast.jsx
@@ -24,7 +24,20 @@ const Podcast = () => {
               We said we'd say it was the snow that killed the other two, but it wasn't. 
               Nature is lethal but it doesn't hold a candle to man.
               </p>
-              <iframe title="brandit. Podcast" src="https://open.spotify.com/embed/show/3JYc6CejghMVvFXCrzZne8?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              <iframe className= "podcast__spotify"
+              title="brandit. Podcast" src="https://open.spotify.com/embed/show/3JYc6CejghMVvFXCrzZne8?utm_source=generator" 
+              width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; 
+              picture-in-picture" loading="lazy"> 
+              </iframe>
+              <p>
+              You think water moves fast? You should see ice. It moves like it has a mind. 
+              Like it knows it killed the world once and got a taste for murder. 
+              After the avalanche, it took us a week to climb out. Now, I don't know exactly 
+              when we turned on each other, but I know that seven of us survived the slide... 
+              and only five made it out. Now we took an oath, that I'm breaking now. 
+              We said we'd say it was the snow that killed the other two, but it wasn't. 
+              Nature is lethal but it doesn't hold a candle to man.
+              </p>
             </div>
           </div>
         </section>
