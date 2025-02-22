5db0c457db793cbfc7dd56356e67c965dc0fbafd
niko87dot <niko.schweres@gmail.com>
2025-02-22 11:57:48 +0100

    Add files via upload

--- src/pages/references/References.jsx
+++ src/pages/references/References.jsx
@@ -1,5 +1,5 @@
 import { projects } from "../../data"
-import Projekte from "../../components/Projekte"
+import Card from "../../UI/Card"
 import Header from "../../components/Header"
 import HeaderImage from "../../images/Header_Referenzen.png"
 import { Link } from 'react-router-dom';
@@ -18,11 +18,20 @@ const References = () => {
             {
               projects.map(({id, title, headline, path, image}) => {
                     return (
-                        <Projekte className="projects__list" key={id} image={image} title={title} headline={headline} path={path}>
-                            <h4>{title}</h4>
-                            <p>{headline}</p>
-                            <Link to={path} className="btn sm">Mehr <AiFillCaretRight/></Link>
-                        </Projekte>
+                            <Card className="project__list" key={id}>
+                                <img src={image} alt={title} />
+                                <h2>{title}</h2>
+                                <p>{headline}</p>
+                                <Link to={path} className="btn sm">Mehr <AiFillCaretRight/></Link>
+                            </Card>
+                            
+                            
+                            
+                            /* <Projekte className="projects__list" key={id} image={image} title={title} headline={headline} path={path}>
+                              <h4>{title}</h4>
+                              <p>{headline}</p>
+                              <Link to={path} className="btn sm">Mehr<AiFillCaretRight/></Link>
+                            </Projekte> */
                       )
                   })
             }
