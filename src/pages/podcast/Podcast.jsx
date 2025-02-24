f6921e661b43c0b811f671e16c574c4b9bfe654c
niko87dot <niko.schweres@gmail.com>
2025-02-24 13:51:55 +0100

    Add files via upload

--- src/pages/podcast/Podcast.jsx
+++ src/pages/podcast/Podcast.jsx
@@ -24,7 +24,7 @@ const Podcast = () => {
               <a href="https://www.amazon.de/music/player/podcasts/eeeaa93e-a7ce-4bcc-8d0a-6fa6ad198261/brandit" target="_blank" rel="noreferrer noopener"><SiAmazonmusic size="40px"/></a>
               <a href="https://podcasts.apple.com/at/podcast/brandit/id1724738282" target="_blank" rel="noreferrer noopener"><SiApplepodcasts size="40px"/></a>
             </div>
-            <p>
+            <p className="podcast_psection">
             Bei brandit. - dem Branding Podcast, sprechen wir wöchentlich über ein neues Thema, wie beispielsweise die Kraft von Storytelling, 
             die Macht eines einprägsamen Logos oder die Geheimnisse erfolgreicher Marketingkampagnen – alles aus der Welt des Brandings. 
             Dabei geht es nicht nur um theoretisches Wissen, sondern auch um praxiserprobte Strategien, inspirierende Geschichten und bewährte Methoden, 
@@ -36,7 +36,9 @@ const Podcast = () => {
         </div>
       </div>
       <div className="container podcast-container2">
-        <p>
+        <p className="pcontenttwo">
+          Ergänzend dazu beleuchten wir in jeder Episode, wie Marken durch konsequente Kommunikation und den gezielten Einsatz moderner Brandingstrategien nicht nur ihre Zielgruppen erreichen, sondern auch nachhaltige Verbindungen aufbauen. Mit tiefgehenden Analysen und praxisnahen Beispielen vermitteln wir, wie authentische Markenidentitäten entstehen und welche Faktoren den langfristigen Erfolg sichern.
+          Darüber hinaus sind unsere Themen so breit gefächert wie die Welt des Brandings selbst. Die Hosts bringen nicht nur ihr fundiertes Expertenwissen mit ein, sondern auch ihre persönliche Meinung und Leidenschaft. In lebhaften Diskussionen wird beispielsweise erörtert, ob es sich für Unternehmen lohnt, bei TikTok-Trends mitzumachen, welcher der über 60 Coca-Cola-Slogans der überzeugendste ist und warum Discounter-Merch plötzlich als cool gilt. So wird bei brandit. nicht nur informiert, sondern auch inspiriert.
         </p>
       </div>
     </section>
