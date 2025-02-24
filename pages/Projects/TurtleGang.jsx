087e4ccc190b6f98e8d44549d7be5675f6e4ff42
niko87dot <niko.schweres@gmail.com>
2025-02-24 18:14:29 +0100

    Add files via upload

--- pages/Projects/TurtleGang.jsx
+++ pages/Projects/TurtleGang.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 
 const TurtleGang = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="project">
+            <div className="container project__container">
+              <h1>Turtle Gang</h1>
+              <h2>Need</h2>
+              <p>
+              Für die NFT-Kollektion Turtle Gang bestand die Aufgabe darin, die komplette <strong>grafische 
+              Gestaltung</strong> und <strong>Generierung</strong> der Kollektion zu übernehmen. Ziel war es, eine große Anzahl 
+              einzigartiger NFTs zu erschaffen und die Kollektion vor dem Launch mit maximaler 
+              Reichweite zu bewerben.
+              </p>
+              <h2>Unsere Herangehensweise:</h2>
+              <p>
+              Wir entwickelten über <strong>2.000 individuelle NFTs</strong> mit verschiedenen Attributen, die jedes 
+              Exemplar einzigartig machten. Neben der visuellen Gestaltung waren wir auch für die 
+              <strong>Verbreitung und Werbung</strong> vor dem Launch verantwortlich, um so viel Aufmerksamkeit wie 
+              möglich zu generieren.
+              </p>
+              <h2>Unsere Lösung</h2>
+              <p>
+              Dazu erstellten wir unter anderem <strong>Motion Graphics-Werbevideos</strong>, die sich durch ihren in 
+              der Szene damals <strong>einzigartigen Retro-Look</strong> auszeichneten und große Aufmerksamkeit 
+              erregten. Ergänzend setzten wir auf <strong>Influencer-Marketing</strong>: Wir übernahmen die 
+              <strong>Koordination und Aufnahme</strong> von Influencern, die die Kollektion vor dem Launch auf 
+              ihren Kanälen bewarben. Durch unsere Arbeit gelang es, die NFT-Kollektion Turtle Gang 
+              in der Community erfolgreich bekannt zu machen und bereits vor dem Launch für großes 
+              Interesse zu sorgen.
+              </p>
+            </div>
+          </section>
   )
 }
 
