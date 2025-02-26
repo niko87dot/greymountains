70a67c85c2343911e55350cf08df2f3bcc097da1
niko87dot <niko.schweres@gmail.com>
2025-02-26 10:29:56 +0100

    Add files via upload

--- pages/AGBs/AGBs.jsx
+++ pages/AGBs/AGBs.jsx
@@ -2,9 +2,54 @@ import "./agbs.css"
 
 const AGBs = () => {
   return (
-    <section className="projects">
-        <div className="container projects__container">
-            <h1>AGbs</h1>
+    <section className="agb">
+        <div className="container agb__container">
+            <h1>AGb</h1>
+            <h2>Allgemeine Geschäftsbedingungen (AGB) der Schweres & Schweres GbR - Grey Mountains</h2>
+            <h3>1. Allgemeines & Geltungsbereich</h3>
+            <p>
+              <li>
+                <ol>Diese Allgemeinen Geschäftsbedingungen (AGB) gelten für alle Verträge, die zwischen Schweres & Schweres GbR - Grey Mountains (im Folgenden "Auftragnehmer") und ihren Kunden (im Folgenden "Auftraggeber") geschlossen werden.</ol>
+                <ol>Abweichende oder entgegenstehende Bedingungen des Auftraggebers werden nicht anerkannt, es sei denn, der Auftragnehmer hat ihrer Geltung ausdrücklich schriftlich zugestimmt.</ol>
+              </li>
+            </p>
+            <h3>2. Vertragsabschluss</h3>
+            <p>
+              <li>
+                <ol>Angebote des Auftragnehmers sind freibleibend und unverbindlich. Ein Angebot ist 30 Tage lang gültig, sofern nicht anders angegeben. Ein Vertrag kommt erst durch schriftliche Bestätigung des Auftragnehmers zustande.</ol>
+                <ol>Änderungen des Leistungsumfangs nach Vertragsabschluss bedürfen einer schriftlichen Vereinbarung.</ol>
+              </li>
+            </p>
+            <h3>3. Leistungen & Leistungsumfang</h3>
+            <p>
+              <li>
+                <ol>Der Leistungsumfang ergibt sich aus dem jeweiligen Angebot oder Vertrag.</ol>
+                <ol>Der Auftragnehmer behält sich das Recht vor, kreative Entscheidungen innerhalb des vereinbarten Rahmens zu treffen</ol>
+                <ol>Zusatzleistungen, die nicht im Angebot enthalten sind, werden gesondert berechnet.</ol>
+                <ol>Das Endprodukt wird nach dem finalen Produktionstag innerhalb von 7 Tagen zur Verfügung gestellt (erste Version), vorausgesetzt, dass alle erforderlichen Assets und Materialien rechtzeitig vom Auftraggeber zur Verfügung gestellt wurden.</ol>
+              </li>
+            </p>
+            <h3>4. Vergütung & Zahlungsbedingungen</h3>
+            <p>
+              <li>
+                <ol>Alle Preise verstehen sich netto zuzüglich der gesetzlichen Umsatzsteuer.</ol>
+                <ol>Zahlungen sind innerhalb von 14 Tagen nach Rechnungsstellung ohne Abzug fällig.</ol>
+                <ol>Bei verspäteter Zahlung können Verzugszinsen und Mahngebühren berechnet werden.</ol>
+              </li>
+            </p>
+            <h3>5. Urheberrechte & Nutzungsrechte</h3>
+            <p>
+              <li>
+                <ol>Alle durch den Auftragnehmer erstellten Videos, Animationen, Konzepte und kreativen Werke unterliegen dem Urheberrecht.</ol>
+                <ol>Der Auftraggeber erhält ein einfaches, nicht übertragbares und nicht unterlizenzierbares Nutzungsrecht für den vereinbarten Verwendungszweck.</ol>
+                <ol>Der Auftraggeber erhält ein einfaches, nicht übertragbares Nutzungsrecht für den vereinbarten Verwendungszweck, insbesondere für die Nutzung zu Werbezwecken auf eigenen Kanälen (Website, Social Media, Messen etc.).<br/>Eine Weiterveräußerung, Unterlizenzierung oder Übertragung der Nutzungsrechte an Dritte ist ohne vorherige schriftliche Zustimmung des Auftragnehmers nicht gestattet.</ol>
+                <ol>Konzepte bleiben geistiges Eigentum des Auftragnehmers, bis sie umgesetzt wurden, und dürfen nicht ohne Erlaubnis an externe Dritte außerhalb des Unternehmens des Auftraggebers weitergegeben oder veröffentlicht werden.</ol>
+                <ol>Jegliche nachträgliche Bearbeitung oder Veränderung der Inhalte ist ohne vorherige schriftliche Zustimmung des Auftragnehmers nicht gestattet.</ol>
+                <ol>Der Auftragnehmer behält sich das Recht vor, die produzierten Inhalte für Eigenwerbung zu nutzen, sofern der Auftraggeber dem nicht widerspricht.</ol>
+                <ol>Rohmaterial wird nicht zur Verfügung gestellt bzw. erfordert eine gesonderte Vereinbarung.</ol>
+                <ol>Rohmaterial wird nach Abschluss des Projekts gelöscht, fertige Projekte werden für 2 Jahre verwahrt.</ol>
+              </li>
+            </p>
           </div>
       </section>
   )
